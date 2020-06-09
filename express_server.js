86ca33ef949914e3b60f0a8e53eb237845c81e0b
Andrew Delmar <andrewjdelmar@gmail.com>
2020-06-09 22:35:07 +0000

    Initial commit

--- express_server.js
+++ express_server.js
@@ -7,9 +7,21 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.tsn.ca"
+  "b2xVn2": "http://www.lighthouselabs.ca/",
+  "9sm5xK": "http://www.google.com/"
 };
+
+//POST route that updates longURL
+app.post("/urls/:shortURL/update", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect('/urls/' + req.params.shortURL);
+})
+
+//POST route that removes URL
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls/');
+})
 
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
@@ -23,6 +35,7 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/:${shortURL}`); 
 });
 
+//create new URL
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -35,21 +48,18 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+//My URL's
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send("Hello! This is the home page.");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
